f3874be69a51dd2dabb6f9f6e04a9fcc75cd83d8
ferenckovecses <48289363+ferenckovecses@users.noreply.github.com>
2020-07-24 12:53:15 +0200

    Projekt feltöltése
    Projekt jelenlegi állásának feltöltése.
    Eddig megvalósított pontok:
    -Kártya osztály SCO
    -Első szériás kártyák elkészítése szkennelt képekkel.
    -Kártya enciklopédia Scene a kártyák adatainak ellenőrzésére.

---
+++ MasterCrok_CCG/Assets/Scripts/CardDex/CardDex_Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDex_Controller : MonoBehaviour
+{
+	public List<Card> cardList = new List<Card>();
+
+	public GameObject cardDisplayPrefab;
+	public GameObject canvasParent;
+
+	List<GameObject> cardDisplayList = new List<GameObject>();
+
+	void Start()
+	{
+		foreach (Card card in cardList) 
+		{
+			GameObject temp = Instantiate(cardDisplayPrefab, new Vector3(0,0,0), Quaternion.identity, canvasParent.transform);
+			temp.GetComponent<Image>().sprite = card.GetArt();
+		}
+		canvasParent.transform.position = new Vector3(0,-1280,0);
+	}
+
+
+
+}
+	
